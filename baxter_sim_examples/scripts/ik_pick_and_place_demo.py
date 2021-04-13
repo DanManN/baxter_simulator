bfaa661582eddc19f02879a0de86246a18cbffdc
Daniel Nakhimovich <dnahimov@gmail.com>
2021-04-13 13:48:38 -0400

    noetic

--- baxter_sim_examples/scripts/ik_pick_and_place_demo.py
+++ baxter_sim_examples/scripts/ik_pick_and_place_demo.py
@@ -26,7 +26,6 @@
 # CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
-
 """
 Baxter RSDK Inverse Kinematics Pick and Place Demo
 """
@@ -60,11 +59,12 @@ from baxter_core_msgs.srv import (
 
 import baxter_interface
 
+
 class PickAndPlace(object):
-    def __init__(self, limb, hover_distance = 0.15, verbose=True):
-        self._limb_name = limb # string
-        self._hover_distance = hover_distance # in meters
-        self._verbose = verbose # bool
+    def __init__(self, limb, hover_distance=0.15, verbose=True):
+        self._limb_name = limb  # string
+        self._hover_distance = hover_distance  # in meters
+        self._verbose = verbose  # bool
         self._limb = baxter_interface.Limb(limb)
         self._gripper = baxter_interface.Gripper(limb)
         ns = "ExternalTools/" + limb + "/PositionKinematicsNode/IKService"
@@ -80,7 +80,7 @@ class PickAndPlace(object):
     def move_to_start(self, start_angles=None):
         print("Moving the {0} arm to start pose...".format(self._limb_name))
         if not start_angles:
-            start_angles = dict(zip(self._joint_names, [0]*7))
+            start_angles = dict(zip(self._joint_names, [0] * 7))
         self._guarded_move_to_joint_position(start_angles)
         self.gripper_open()
         rospy.sleep(1.0)
@@ -92,8 +92,8 @@ class PickAndPlace(object):
         ikreq.pose_stamp.append(PoseStamped(header=hdr, pose=pose))
         try:
             resp = self._iksvc(ikreq)
-        except (rospy.ServiceException, rospy.ROSException), e:
-            rospy.logerr("Service call failed: %s" % (e,))
+        except (rospy.ServiceException, rospy.ROSException) as e:
+            rospy.logerr("Service call failed: %s" % (e, ))
             return False
         # Check if result valid, and type of seed ultimately used to get solution
         # convert rospy's string representation of uint8[]'s to int's
@@ -101,13 +101,12 @@ class PickAndPlace(object):
         limb_joints = {}
         if (resp_seeds[0] != resp.RESULT_INVALID):
             seed_str = {
-                        ikreq.SEED_USER: 'User Provided Seed',
-                        ikreq.SEED_CURRENT: 'Current Joint Angles',
-                        ikreq.SEED_NS_MAP: 'Nullspace Setpoints',
-                       }.get(resp_seeds[0], 'None')
+                ikreq.SEED_USER: 'User Provided Seed',
+                ikreq.SEED_CURRENT: 'Current Joint Angles',
+                ikreq.SEED_NS_MAP: 'Nullspace Setpoints',
+            }.get(resp_seeds[0], 'None')
             if self._verbose:
-                print("IK Solution SUCCESS - Valid Joint Solution Found from Seed Type: {0}".format(
-                         (seed_str)))
+                print("IK Solution SUCCESS - Valid Joint Solution Found from Seed Type: {0}".format((seed_str)))
             # Format solution into Limb API-compatible dictionary
             limb_joints = dict(zip(resp.joints[0].name, resp.joints[0].position))
             if self._verbose:
@@ -143,12 +142,12 @@ class PickAndPlace(object):
         # retrieve current pose from endpoint
         current_pose = self._limb.endpoint_pose()
         ik_pose = Pose()
-        ik_pose.position.x = current_pose['position'].x 
-        ik_pose.position.y = current_pose['position'].y 
+        ik_pose.position.x = current_pose['position'].x
+        ik_pose.position.y = current_pose['position'].y
         ik_pose.position.z = current_pose['position'].z + self._hover_distance
-        ik_pose.orientation.x = current_pose['orientation'].x 
-        ik_pose.orientation.y = current_pose['orientation'].y 
-        ik_pose.orientation.z = current_pose['orientation'].z 
+        ik_pose.orientation.x = current_pose['orientation'].x
+        ik_pose.orientation.y = current_pose['orientation'].y
+        ik_pose.orientation.z = current_pose['orientation'].z
         ik_pose.orientation.w = current_pose['orientation'].w
         joint_angles = self.ik_request(ik_pose)
         # servo up from current pose
@@ -181,36 +180,38 @@ class PickAndPlace(object):
         # retract to clear object
         self._retract()
 
-def load_gazebo_models(table_pose=Pose(position=Point(x=1.0, y=0.0, z=0.0)),
-                       table_reference_frame="world",
-                       block_pose=Pose(position=Point(x=0.6725, y=0.1265, z=0.7825)),
-                       block_reference_frame="world"):
+
+def load_gazebo_models(
+    table_pose=Pose(position=Point(x=1.0, y=0.0, z=0.0)),
+    table_reference_frame="world",
+    block_pose=Pose(position=Point(x=0.6725, y=0.1265, z=0.7825)),
+    block_reference_frame="world"
+):
     # Get Models' Path
-    model_path = rospkg.RosPack().get_path('baxter_sim_examples')+"/models/"
+    model_path = rospkg.RosPack().get_path('baxter_sim_examples') + "/models/"
     # Load Table SDF
     table_xml = ''
-    with open (model_path + "cafe_table/model.sdf", "r") as table_file:
-        table_xml=table_file.read().replace('\n', '')
+    with open(model_path + "cafe_table/model.sdf", "r") as table_file:
+        table_xml = table_file.read().replace('\n', '')
     # Load Block URDF
     block_xml = ''
-    with open (model_path + "block/model.urdf", "r") as block_file:
-        block_xml=block_file.read().replace('\n', '')
+    with open(model_path + "block/model.urdf", "r") as block_file:
+        block_xml = block_file.read().replace('\n', '')
     # Spawn Table SDF
     rospy.wait_for_service('/gazebo/spawn_sdf_model')
     try:
         spawn_sdf = rospy.ServiceProxy('/gazebo/spawn_sdf_model', SpawnModel)
-        resp_sdf = spawn_sdf("cafe_table", table_xml, "/",
-                             table_pose, table_reference_frame)
-    except rospy.ServiceException, e:
+        resp_sdf = spawn_sdf("cafe_table", table_xml, "/", table_pose, table_reference_frame)
+    except rospy.ServiceException as e:
         rospy.logerr("Spawn SDF service call failed: {0}".format(e))
     # Spawn Block URDF
     rospy.wait_for_service('/gazebo/spawn_urdf_model')
     try:
         spawn_urdf = rospy.ServiceProxy('/gazebo/spawn_urdf_model', SpawnModel)
-        resp_urdf = spawn_urdf("block", block_xml, "/",
-                               block_pose, block_reference_frame)
-    except rospy.ServiceException, e:
+        resp_urdf = spawn_urdf("block", block_xml, "/", block_pose, block_reference_frame)
+    except rospy.ServiceException as e:
         rospy.logerr("Spawn URDF service call failed: {0}".format(e))
+
 
 def delete_gazebo_models():
     # This will be called on ROS Exit, deleting Gazebo models
@@ -221,8 +222,9 @@ def delete_gazebo_models():
         delete_model = rospy.ServiceProxy('/gazebo/delete_model', DeleteModel)
         resp_delete = delete_model("cafe_table")
         resp_delete = delete_model("block")
-    except rospy.ServiceException, e:
+    except rospy.ServiceException as e:
         rospy.loginfo("Delete Model service call failed: {0}".format(e))
+
 
 def main():
     """RSDK Inverse Kinematics Pick and Place Example
@@ -250,34 +252,28 @@ def main():
     rospy.wait_for_message("/robot/sim/started", Empty)
 
     limb = 'left'
-    hover_distance = 0.15 # meters
+    hover_distance = 0.15  # meters
     # Starting Joint angles for left arm
-    starting_joint_angles = {'left_w0': 0.6699952259595108,
-                             'left_w1': 1.030009435085784,
-                             'left_w2': -0.4999997247485215,
-                             'left_e0': -1.189968899785275,
-                             'left_e1': 1.9400238130755056,
-                             'left_s0': -0.08000397926829805,
-                             'left_s1': -0.9999781166910306}
+    starting_joint_angles = {
+        'left_w0': 0.6699952259595108,
+        'left_w1': 1.030009435085784,
+        'left_w2': -0.4999997247485215,
+        'left_e0': -1.189968899785275,
+        'left_e1': 1.9400238130755056,
+        'left_s0': -0.08000397926829805,
+        'left_s1': -0.9999781166910306
+    }
     pnp = PickAndPlace(limb, hover_distance)
     # An orientation for gripper fingers to be overhead and parallel to the obj
-    overhead_orientation = Quaternion(
-                             x=-0.0249590815779,
-                             y=0.999649402929,
-                             z=0.00737916180073,
-                             w=0.00486450832011)
+    overhead_orientation = Quaternion(x=-0.0249590815779, y=0.999649402929, z=0.00737916180073, w=0.00486450832011)
     block_poses = list()
     # The Pose of the block in its initial location.
     # You may wish to replace these poses with estimates
     # from a perception node.
-    block_poses.append(Pose(
-        position=Point(x=0.7, y=0.15, z=-0.129),
-        orientation=overhead_orientation))
+    block_poses.append(Pose(position=Point(x=0.7, y=0.15, z=-0.129), orientation=overhead_orientation))
     # Feel free to add additional desired poses for the object.
     # Each additional pose will get its own pick and place.
-    block_poses.append(Pose(
-        position=Point(x=0.75, y=0.0, z=-0.129),
-        orientation=overhead_orientation))
+    block_poses.append(Pose(position=Point(x=0.75, y=0.0, z=-0.129), orientation=overhead_orientation))
     # Move to the desired starting angles
     pnp.move_to_start(starting_joint_angles)
     idx = 0
@@ -285,9 +281,10 @@ def main():
         print("\nPicking...")
         pnp.pick(block_poses[idx])
         print("\nPlacing...")
-        idx = (idx+1) % len(block_poses)
+        idx = (idx + 1) % len(block_poses)
         pnp.place(block_poses[idx])
     return 0
+
 
 if __name__ == '__main__':
     sys.exit(main())
